a27b91609477b9fc21c9e106ba4335a97e34a0e3
darinel <darinelcigarroa97@gmail.com>
2021-11-05 16:59:21 -0600

    :lipstick: Agregue estilos y funcionalidad a la vista de stickers

---
+++ src/components/StickerCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      loader="lazy"
+      :src="sticker.images.downsized.url"
+      class="card-img-top"
+      :alt="sticker.title"
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ sticker.title }}</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <!-- <a :href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stickercard",
+  props: {
+    sticker: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/Stickers.vue
+++ src/views/Stickers.vue
@@ -1,9 +1,59 @@
 <template>
-    <h1>Stickers</h1>
+  <div class="container mt-3">
+    <h1 class="text-center">Stickers</h1>
+    <hr />
+    <div class="search">
+      <Search @searchCategory="getGifs" />
+    </div>
+    <div class="row">
+      <div class="content-card">
+        <div v-show="loading" class="loading">
+            <Loading />
+        </div>
+        <StickerCard v-for="sticker in stickers" :key="sticker.id" :sticker="sticker" class="m-3" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default{
-    name: "Stickers"
-}
+import StickerCard from "@/components/StickerCard.vue";
+import Search from "../components/Search.vue";
+import Loading from "../components/Loading.vue";
+
+export default {
+  name: "Stickers",
+  components: { StickerCard, Search, Loading },
+  data: () => ({
+    stickers: {},
+    loading: null,
+  }),
+  methods: {
+    async getGifs(search = "naruto") {
+      this.loading = true;
+      const { data } = await this.axios.get(
+        `https://api.giphy.com/v1/stickers/search?q=${search}&api_key=aFNFkgVW4NfEgRc3oq3MrGwU56oVnCjh`
+      );
+      this.stickers = data.data;
+      this.loading = false;
+    },
+  },
+  created() {
+    this.getGifs();
+  },
+};
 </script>
+
+<style scoped>
+.search {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content-card {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+</style>
