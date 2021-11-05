21c632d9fb350b5e161b5ae2cdb89425e9117c4c
darinel <darinelcigarroa97@gmail.com>
2021-11-04 21:37:44 -0600

    :rocket: Implemente navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Header />
   </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style>
 @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
 </style>
