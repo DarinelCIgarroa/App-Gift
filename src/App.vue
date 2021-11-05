f4b0607eb25098c6a113fe8479f360ed0faaba15
darinel <darinelcigarroa97@gmail.com>
2021-11-04 21:18:51 -0600

    :tada: subiendo proyecto

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,7 @@
   </div>
   <router-view/>
 </template>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
